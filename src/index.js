b92b2a4c26738e12794f50be25beb4780147b03a
FernandoDavid <prepa.2001@gmail.com>
2021-08-09 07:39:52 -0500

    Final project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const {createWindow} = require('./main.js')
 const {app} = require('electron')
 const path = require('path')
-const bootstrap = require('bootstrap');
 
 require('electron-reload')(__dirname)
 
