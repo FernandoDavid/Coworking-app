7f6c826e933386908c7828abddcec63518023b7c
FernandoDavid <prepa.2001@gmail.com>
2021-08-09 10:20:44 -0500

    Base state (Before Hbs)

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ const {BrowserWindow} = require('electron');
 let window;  
 
 function createWindow(){
-        window = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                nodeIntegration: true
-            }
-        });
+        window = new BrowserWindow({ show:false });
+        window.maximize();
         window.loadFile('src/views/index.html');
+        window.show();
 }
 
 module.exports={
