5592bdfeea6a5269276d4ffdcb6a182290436e6b
FernandoDavid <prepa.2001@gmail.com>
2021-08-09 12:58:13 -0500

    Structure minor change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-const {createWindow} = require('./main.js')
-const {app} = require('electron')
+const {BrowserWindow, app} = require('electron')
 const path = require('path')
 
-require('electron-reload')(__dirname)
+let window;
 
 require('./database.js');
+require('electron-reload')(__dirname)
+
+function createWindow(){
+    window = new BrowserWindow({ show:false });
+    window.maximize();
+    window.loadFile('src/views/index.html');
+    window.show();
+    window.on("closed", () => {
+        app.quit()
+    });
+}
+
+app.on("quit", () => {
+    newHandlebars.clearTemps();
+});
+
+
 
 app.whenReady().then(createWindow);
